5c375a26708add0321fbf9853ba6ee64396714a9
sdaksnavjvla <74776719+sdaksnavjvla@users.noreply.github.com>
2020-11-20 20:19:38 +0200

    haide

--- mat.py
+++ mat.py
@@ -1,1 +1,16 @@
-print("giuco se indentifica drept o masa de calacat")
+import sqlite3 as lit
+
+def main():
+
+    try:
+        db=lit.connect("data2.db")
+        cur=db.cursor()
+        query="CREATE TABLE users (id INT, nume TEXT, adresa TEXT)"
+        cur.execute(query)
+        print("s-a creat")
+    except:
+        print("nu s-a creat tabel")
+    finally:
+        db.close()
+
+main()
