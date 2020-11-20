28f37aef5e83492d62c907d3c8290731a22f9e87
fanvancea <74776718+fanvancea@users.noreply.github.com>
2020-11-21 00:38:20 +0200

    mesaj

--- app.py
+++ app.py
@@ -1,65 +1,16 @@
-from flask import Flask, render_template, url_for, request, redirect
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-db = SQLAlchemy(app)
-
-class Todo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200), nullable=False)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
-
-    def __repr__(self):
-        return '<Task %r>' % self.id
+import flask
+from flask import request, url_for, render_template, redirect
 
 
-@app.route('/', methods=['POST', 'GET'])
-def index():
-    if request.method == 'POST':
-        task_content = request.form['content']
-        new_task = Todo(content=task_content)
+app = flask.Flask(__name__)
 
-        try:
-            db.session.add(new_task)
-            db.session.commit()
-            return redirect('/')
-        except:
-            return 'There was an issue adding your task'
+@app.route('/',methods=['GET','POST'])
+def my_maps():
 
-    else:
-        tasks = Todo.query.order_by(Todo.date_created).all()
-        return render_template('index.html', tasks=tasks)
+  mapbox_access_token = 'pk.eyJ1IjoiYW1vc2FuZHJlaWNhIiwiYSI6ImNraHF1Mzk3OTBiMWwycG10bTdldHRqcG8ifQ.yRwvlAqvEeZMcpjYFrLpAw'
 
+  return render_template('index.html',
+        mapbox_access_token=mapbox_access_token)
 
-@app.route('/delete/<int:id>')
-def delete(id):
-    task_to_delete = Todo.query.get_or_404(id)
-
-    try:
-        db.session.delete(task_to_delete)
-        db.session.commit()
-        return redirect('/')
-    except:
-        return 'There was a problem deleting that task'
-
-@app.route('/update/<int:id>', methods=['GET', 'POST'])
-def update(id):
-    task = Todo.query.get_or_404(id)
-
-    if request.method == 'POST':
-        task.content = request.form['content']
-
-        try:
-            db.session.commit()
-            return redirect('/')
-        except:
-            return 'There was an issue updating your task'
-
-    else:
-        return render_template('update.html', task=task)
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
